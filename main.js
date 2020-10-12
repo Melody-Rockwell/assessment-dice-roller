0eb26740947a91b411fb39bf3f0aa1adb773f499
Melody <melody.rockwell3@gmail.com>
2020-10-12 15:43:34 -0400

    finished assessment

--- main.js
+++ main.js
@@ -1,48 +1,39 @@
-//you will total the results of all the dice that were
-// rolled and add the sum to the "Total" area on the page using innerHTML.
 let numberOfDiceEntered = document.querySelector("#number-of-dice-entered");
 let roll = document.querySelector("#roll");
-let showAllRolls = document.querySelector("#show-all-rolls");
-let totalOfDiceResults = document.querySelector("#totalOfDiceResults");
-let allRollsMade = document.querySelector("allRollsMade");
+let showAllRollsButton = document.querySelector("#show-all-rolls");
+let totalOfDiceResults = document.querySelector("#total-of-dice-results");
+let rollResults = document.querySelector("#roll-results");
+let dieRolls = [];
 
 roll.addEventListener("click", function () {
   let amountOfDice = numberOfDiceEntered.value;
   let count = 0;
-  let dieRolls = [];
+  dieRolls = [];
   while (count < amountOfDice) {
     dieRolls.push(randomRoll());
-    console.log(count);
     count++;
   }
+  count = 0;
+  let total = 0;
+
+  while (count < dieRolls.length) {
+    total += dieRolls[count];
+    console.log(dieRolls[count]);
+    count++;
+  }
+  totalOfDiceResults.innerHTML = "The total of all rolls is: " + total;
   console.log(dieRolls);
 });
 function randomRoll() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-// showAllRolls.addEventListener("click", function () {
-//   let count = 0;
-//   let dieRolls = [];
-//   while (count < dieRolls.length) {
-//     console.log(dieRolls[count]);
-//     count++;
-//   }
-// });
-
-//
-
-//
-
-// }
-
-// Define an empty array, named "dieRolls".
-// Add a click event handler to the "Roll!' button.
-// In the event handler, get the requested number of dice from the user and assign it to a new variable to use later.
-// Using a loop, simulate die rolls a number of times according to the number requested by the user.
-// hint: user will input a # of dice and click roll
-
-// In the loop, push each die roll result to the end of the dieRolls array.
-
-// Sum the results from the dieRolls array, and display the total in the Total element using innerHTML.
-// Add an event listener to the "Show All Rolls" button, which loops over the dieRolls array, to get each individual die roll and display it as a List Item within the "All Rolls" element.
+showAllRollsButton.addEventListener("click", function () {
+  rollResults.innerhtml = "";
+  let count = 0;
+  while (count < dieRolls.length) {
+    const newString = '<li class="roll-results">' + dieRolls[count] + "</li>";
+    rollResults.innerHTML += newString;
+    count++;
+  }
+});
